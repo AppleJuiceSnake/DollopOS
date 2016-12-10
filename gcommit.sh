294d508725dd8cd8f1ed4a6736b88ad9c2795969
johnnyw3 <johnathan8080@gmail.com>
2016-12-10 16:22:25 -0600

    added final sleep

--- gcommit.sh
+++ gcommit.sh
@@ -15,6 +15,8 @@ if ! git diff-index --quiet HEAD --; then
     git push
     rm gitmsg
     printf "Finished commiting and merging of repo.\n All done."
+	sleeptime=10
+	sleep
 else
     printf "There's nothing to commit, so make some changes to commitable files or make sure they're not being ignored%s\n" "!"
     printf "." && sleep 1 && printf "." && sleep 1 && printf ".\n" && sleep 1
