f686e13a33c3425b23b5e44e6efbdcb16f38b3ea
johnnyw3 <johnathan8080@gmail.com>
2016-12-10 09:03:22 -0600

    gitmsg

--- main.py
+++ main.py
@@ -3,4 +3,10 @@ import os
 import sys
 import pygame
 
+#set up screen
+pygame.init()
+screen = pygame.display.set_mode((320, 480))
+
+
+# a test comment left in the code; should remove
 print "Dollop OS Testing Git or Whatevs"
