647c66699495431d5323f4f2c321cc419c02b3a9
Atmatm6 <atmatm6000@gmail.com>
2016-12-09 21:27:16 -0600

    gitmsg

--- gcommit.sh
+++ gcommit.sh
@@ -12,4 +12,11 @@ if ! git diff-index --quiet HEAD --; then
     printf "Finished commiting and merging of repo.\n"
 else
     printf "There's nothing to commit, so make some changes to commitable files or make sure they're not being ignored!\n"
+    printf "." && sleep 0.5 && printf "." && sleep 0.5 && printf ".\n" && sleep 0.5 
+    printf "But, even if there's nothing to commit, we're going to still attempt a merge!\n"
+    if git merge; then
+        printf "Finished merging of repo.\n"
+    else
+        printf "Merge errored.\n"
+    fi
 fi
