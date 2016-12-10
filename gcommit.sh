de4818b94a531d30bde6fa05d9618ed1e242c0be
Atmatm6 <atmatm6000@gmail.com>
2016-12-09 21:07:59 -0600

    gitmsg

---
+++ gcommit.sh
@@ -0,0 +1,15 @@
+git add .
+if ! git diff-index --quiet HEAD --; then
+    printf "Please write the git commit message.\n"
+    sleep 2
+    printf "#Git Files Being Committed:\n" > gitmsg
+    for i in $(git diff --name-only); do
+        printf "\#\t%s\n" "$i"
+    done
+    nano gitmsg
+    git commit -m gitmsg
+    git merge
+    printf "Finished commiting and merging of repo.\n"
+else
+    printf "There's nothing to commit, so make some changes to commitable files or make sure they're not being ignored!\n"
+fi
