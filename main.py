c2e1dca9ee7bac6a7a107658158b78edd874d48c
johnnyw3 <johnathan8080@gmail.com>
2016-12-10 16:04:26 -0600

    Added potentional background...

--- main.py
+++ main.py
@@ -8,11 +8,16 @@ pygame.init()
 screen = pygame.display.set_mode((320, 480))
 done = False
 
+index = pygame.image.load('placeholder.png')
+screen.blit(index, (1, 1))
+pygame.display.flip()
+
 while not done:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
     
+
 
 # a test comment left in the code; should remove
 print "Dollop OS Testing Git or Whatevs"
