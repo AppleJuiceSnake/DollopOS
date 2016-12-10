b4e3276b5e2f7c66ad15294008fcdbed144e601d
johnnyw3 <johnathan8080@gmail.com>
2016-12-10 16:18:15 -0600

    added varous little tweaks

--- README.md
+++ README.md
@@ -3,4 +3,6 @@ Apple Juice Snake's Operating System
 
 The first letters of Apple Juice are the first letters of the team's first names, and the snake stands for Python.
 
+For more info, please visit Dollop-OS's website at http://dollop.texttale.ga
+
 Python is a must, but PyGame is required for this project.
--- main.py
+++ main.py
@@ -8,16 +8,22 @@ pygame.init()
 screen = pygame.display.set_mode((320, 480))
 done = False
 
-index = pygame.image.load('placeholder.png')
+#Background Image
+index = pygame.image.load('res/placeholder.png')
 screen.blit(index, (1, 1))
 pygame.display.flip()
 
+#Speed Control
+clock = pygame.time.Clock()
+FRAMES_PER_SECOND = 30
+deltat = clock.tick(FRAMES_PER_SECOND)
+
+#Quitting Control
 while not done:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
     
-
 
 # a test comment left in the code; should remove
 print "Dollop OS Testing Git or Whatevs"
