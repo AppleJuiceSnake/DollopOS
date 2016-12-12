c73590a9fc0e44833a76cd666e97c7ab96c4861e
Atmatm6 <atmatm6000@gmail.com>
2016-12-11 18:38:10 -0600

    moved everything out of __init__.py

--- start.py
+++ start.py
@@ -1,11 +1,6 @@
-Python 2.7.12 (default, Nov 19 2016, 06:48:10) 
-[GCC 5.4.0 20160609] on linux2
-Type "copyright", "credits" or "license()" for more information.
->>> import os
->>> os.getcwd
-<built-in function getcwd>
->>> os.getcwd()
-'/home/atmatm6/dollop-os'
->>> execfile("main.py")
-Incomplete!~
->>>
+#!/usr/bin/python
+import os
+import sys
+print os.getcwd()
+nothing = input()
+execfile("main.py")
